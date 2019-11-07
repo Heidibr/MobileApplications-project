23b13075b091a2fcf2e1b43f690adca23bcadf7f
odasko <odaskoglund7@gmail.com>
2019-11-07 14:48:29 +0100

    firebase

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApiKeys from './constants/ApiKeys';
+import * as firebase from 'firebase';
 
-export default function App() {
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initialize firebase
+    firebase.initializeApp(ApiKeys.firebaseConfig);
+  }
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up !!!! App.tsx</Text>
+      <Text>Open up App.tsx</Text>
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
---
+++ constants/ApiKeys.js
@@ -0,0 +1,12 @@
+export default {
+    firebaseConfig: {
+        apiKey: "AIzaSyB5ljieOp413qery-bh279woY3_gn3ND20",
+        authDomain: "mobileapp-416ea.firebaseapp.com",
+        databaseURL: "https://mobileapp-416ea.firebaseio.com",
+        projectId: "mobileapp-416ea",
+        storageBucket: "mobileapp-416ea.appspot.com",
+        messagingSenderId: "145090313122",
+        appId: "1:145090313122:web:647a7a81c4fc85a90910fb",
+        measurementId: "G-R14C90Z161"
+      },
+}
